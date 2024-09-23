60552a37f32ac43755556e4d0ddbd7e2d41b8cec
tanyamaheshwari15 <tanya.maheshwari15nov>
2024-09-23 12:58:52 +0530

    js project

--- 07/questions.js
+++ 07/questions.js
@@ -1,2 +1,30 @@
 //using selectors inside the elements
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function(question){
+    // console.log(question);
+    const btn = question.querySelector(".question-btn");
+    // console.log(btn);.
+    btn.addEventListener("click", function(){
+
+        questions.forEach(function(item){
+            if(item !== question){
+                item.classList.remove("show-text");
+            }
+        });
+
+        question.classList.toggle("show-text");
+    });
+});
+
+//OR you can use...
 //traversing the dom
+
+// const btns = document.querySelectorAll(".question-btn");
+// btns.forEach( function(btn) {
+//     btn.addEventListener("click", function(e){
+//         const question = e.currentTarget.parentElement.parentElement
+//         question.classList.toggle("show-text");
+//     });
+// });
